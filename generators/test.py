a800bfa75cfe02606d9bd50573adcf012881e492
Timer <konkin_anton@list.ru>
2020-11-30 13:29:28 +0400

    Доделал генератор с электронными уровнями.

--- generators/test.py
+++ generators/test.py
@@ -1,16 +1,29 @@
 import random
 
-t = 'Определите, атомы каких двух из указанных в ряду элементов имеют на внешнем энергетическом уровне {} электрона.\n{}'
+t = 'Определите, атомы каких двух из указанных в ряду элементов имеют на внешнем энергетическом уровне {} электрона(ов).\n{}'
 subject_name = "Химия"
 theme_name = "Электронная конфигурация атома"
+l = [['Li','Na','K','Rb','Cs'], ['Be', 'Mg', 'Ca', 'Sr', 'Ba'], ['B', 'Al','Ga', 'In', 'Tl'], ['C', 'Si', 'Ge', 'Sn', 'Pb'], ['N', 'P', 'As', 'Sb', 'Bi'], ['O', 'S', 'Se', 'Te'], ['F', 'Cl', 'Br', 'I']]
+
 
 def generate():
-    tmp = [[['один','четыре'], 
-'''1. Na
-2. K
-3. Si
-4. Mg
-5. C''', ['12','35']]]
-    a = random.randint(0,1)
-    b = tmp[0][1]
-    return (t.format(tmp[0][0][a],b), tmp[0][2][a])
+    correct = random.randint(0,6)
+    a = [correct, correct]
+    for i in range(3):
+        tmp = random.randint(0,6)
+        while tmp == correct:
+            tmp = random.randint(0,6)
+        a.append(tmp)
+    random.shuffle(a)
+    ans = []
+    for i,j in enumerate(a):
+        if j == correct:
+            ans.append(i+1)
+    ans = ''.join(map(str,ans))
+    b = [random.choice(l[i]) for i in a]
+    test = '\n'.join([f'{i+1}. {j}' for i,j in enumerate(b)])
+    return (t.format(correct+1, test), ans)
+    
+
+if __name__ == "__main__":
+    print(generate()[0])
